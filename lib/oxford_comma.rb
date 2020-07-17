185585a11b1739bedff44664c6458bddaac1f4f2
Clayton Moore <cmoorefour@gmail.com>
2020-07-17 18:33:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
- array.join(" , ")
+
+  if (array.length == 1)
+   return array.join(",")
+
+ elsif (array.length == 2)
+   return array.join(" ")
+
+ elsif (array.length == 3)
+   return array.join(" ")
+else(array.length > 3)
+  return array.join(" ")
+
+  end
  end
