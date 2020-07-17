d68aa1db811c28d536fabfe3124f4de888908bbc
Clayton Moore <cmoorefour@gmail.com>
2020-07-17 19:30:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def oxford_comma(array)
 
   if (array.length == 1)
@@ -5,6 +6,7 @@ def oxford_comma(array)
  elsif (array.length == 2)
    return array.join(" and ")
 elsif (array.length == 3)
+  binding.pry
    return array.join("  ")
 elsif(array.length >= 3)
   return array.join(" ")
