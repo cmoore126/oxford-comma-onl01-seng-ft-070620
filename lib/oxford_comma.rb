5b774d0c9efeecf4e56bdf703430d0bba335b5d0
Clayton Moore <cmoorefour@gmail.com>
2020-07-17 19:29:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,7 @@ def oxford_comma(array)
 
  elsif (array.length == 2)
    return array.join(" and ")
-
- elsif (array.length == 3)
+elsif (array.length == 3)
    return array.join("  ")
 elsif(array.length >= 3)
   return array.join(" ")
