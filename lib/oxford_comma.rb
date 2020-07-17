beec6c0b58a341aaac8e4fa90da855e44c4ad7ed
Clayton Moore <cmoorefour@gmail.com>
2020-07-17 19:46:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,10 @@
-require 'pry'
 def oxford_comma(array)
 
   if (array.length == 1)
    return array.join(",")
  elsif (array.length == 2)
    return array.join(" and ")
-else 
-  #binding.pry
+else
   array[-1]= "and " + array[-1]
    return array.join(", ")
 
